d69c91fcae53441fba14a6ba749f227360b32347
Gabriel Okolo <Gabriel.okolo@uws.ac.uk>
2020-12-07 21:36:25 +0000

    first commit

---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\77901248\onedrive - university of the west of scotland\desktop\carzone-project\env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
